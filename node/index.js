d882300bda3637922d90b4dc83433a4887b965ba
claude-hub <314705487@qq.com>
2024-07-02 23:54:58 +0800

    feat: add fbneo gamelist

--- node/index.js
+++ node/index.js
@@ -2,10 +2,23 @@
  * @Author: 314705487@qq.com
  * @Description: 
  * @Date: 2024-06-30 19:43:42
- * @LastEditTime: 2024-07-02 22:55:23
+ * @LastEditTime: 2024-07-02 23:53:40
  */
+const { translate } = require('@vitalets/google-translate-api');
 const { renameXML } = require('./rename');
 const { convert } = require('./convert');
 
 // convert();
 renameXML();
+
+// async function translateToChinese(text) {
+//     try {
+//         const result = await translate(text, { to: 'zh-CN' });
+//         console.log(result.text);
+//     } catch (error) {
+//         console.error('Translation error:', error);
+//     }
+// }
+
+// // 测试
+// translateToChinese('Hello, how are you?');
